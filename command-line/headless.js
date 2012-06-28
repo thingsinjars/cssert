0c6cf440fb14422ec38346abe355a9d9388a6134
Simon Madine <madine@bf-dhcp00913.europe.nokia.com>
2012-06-28 17:22:17 +0200

    Restructure, optimize and loads of comments
    Removing some of the odd behaviors and simplifying things to do only
    what they are expected to do.

--- command-line/headless.js
+++ command-line/headless.js
@@ -1,53 +1,67 @@
-/* cssert - headless.js
- * version 1.0
- *
- * (C) Simon Madine (@thingsinjars) - Mit Style License
- * CSS verification test framework
- * @see http://thingsinjars.github.com/cssert
- *
- * PhantomJS headless control script
- *
- * This loads the same test file as in the browser
- * but instead of running the tests in iframes,
- * they are run in separate browser pages
- */
+/*globals phantom, require, CSSERT */
+var H = {};
+(function() {
+  /* cssert - headless.js
+   * version 1.0
+   *
+   * (C) Simon Madine (@thingsinjars) - Mit Style License
+   * CSS verification test framework
+   * @see http://thingsinjars.github.com/cssert
+   *
+   * PhantomJS headless control script
+   *
+   * This loads the same test file as in the browser
+   * but instead of running the tests in iframes,
+   * they are run in separate browser pages
+   */
 
-var fs = require('fs'),
-  verbose = false,
+  var fs = require('fs'),
+    init, runTest, bufferTest, runTheTests, verbose = true,
+    log_LEVEL = 2,
 
-  // Application stages
-  CREATING_TESTS = 1,
-  RUNNING_TESTS = 2,
-  FINISHED = 3,
-  stage,
+    // Application stages
+    CREATING_TESTS = 1,
+    RUNNING_TESTS = 2,
+    FINISHED = 3,
+    stage,
 
-  // This is the page object in which we run our tests
-  page = require('webpage').create(),
+    // This is the page object in which we run our tests
+    page = require('webpage').create(),
 
-  // To make the test creation/running synchronous
-  outstandingTests = 0,
-  consoleMessage = function(msg) {
-    console.log(msg);
-  },
+    // To make the test creation/running synchronous
+    outstandingTests = 0,
+    consoleMessage = function(msg) {
+      console.log(msg);
+    },
 
-  // All the tests we find in the passed in test runner.
-  tests = [];
+    // All the tests we find in the passed in test runner.
+    tests = [];
 
-// Pipe console logs from host page to command-line
-page.onConsoleMessage = consoleMessage;
+    page.settings.userAgent = 'CSSERT';
 
-// Make sure we are called with at least one test runner
-if (phantom.args.length === 0) {
-  console.log('Usage: cssert testcase.html');
-  phantom.exit();
-} else {
-  stage = CREATING_TESTS;
-}
+  var log = function(message, level) {
+      var colors = ['\033[0;34m', '\033[0;31m', '\033[0;32m', '\033[0;33m', '\033[0;37m'],
+        component = 'HEADLESS :: \033[0;37m';
+      level = level || 0;
+      message = message.replace(/\{(\d)\}/g, '\033[0;3$1m');
+      if (log_LEVEL >= level) {
+        console.log(colors[level] + component + message);
+      }
 
-// Create the test files and begin running the tests
-page.open(phantom.args[0], function(status) {
-  // make sure we only create the test files once.
-  if (stage === CREATING_TESTS) {
+    };
+
+  // Pipe console logs from host page to command-line
+  page.onConsoleMessage = consoleMessage;
+
+  // Make sure we are called with at least one test runner
+  if (phantom.args.length === 0) {
+    log('Usage: cssert testcase.html');
+    phantom.exit();
+  }
+
+  init = function(status) {
+    log('Page Open', 2);
+    // make sure we only create the test files once.
     if (status === "success") {
 
       // Inject jQuery and cssert into the test page so we can test the styles
@@ -65,100 +79,114 @@ page.open(phantom.args[0], function(status) {
       // These will be read individually rather than injecting the content 
       // directly into the page object.
       for (var a = 0; a < tests.length; a++) {
-        testObject = tests[a];
-        if(verbose) {
-          console.log('\033[0;33mCreating runner:\033[0;37m '+testObject.title);
+        // testObject = tests[a];
+        if (verbose) {
+          log('{3}Creating runner:{7} ' + tests[a].title, 3);
         }
-        fs.write('test-' + testObject.title + '.html', testObject.unit, 'w');
-        // beginTest('test-' + testObject.title + '.html', testObject.selector, JSON.parse(testObject.styles), testObject.title);
+        fs.write('test-' + tests[a].title + '.html', tests[a].unit, 'w');
+        bufferTest('test-' + tests[a].title + '.html', tests[a].selector, JSON.parse(tests[a].styles), tests[a].title);
       }
     } else {
-      console.log('\033[0;31mCSSERT : Failed to create runner files');
+      log('Failed to create runner files', 1);
+      log('{1}CSSERT : Failed to create runner files');
     }
 
-    stage = RUNNING_TESTS;
+    log('Running ' + tests.length + ' test'+(tests.length===1?'':'s'), 2);
 
-  } else if (stage === RUNNING_TESTS) {
-    runTheTests();
-    stage = FINISHED;
-  }
-});
+    // for (var a = 0; a < tests.length; a++) {
+    //   runTest('test-' + tests[a].title + '.html', tests[a].selector, JSON.parse(tests[a].styles), tests[a].title);
+    // }
 
-runTest = function(filename, testSubject, stylesToAssert, testTitle) {
-  var page = require('webpage').create();
-
-  // Arbitrary. Can be changed but suits most purposes.
-  page.viewportSize = {
-    width: 1600,
-    height: 1600
-  };
-  page.onConsoleMessage = consoleMessage;
-
-  page.onLoadStarted = function() {};
-
-  page.onLoadFinished = function(status) {
-    if (status === "success") {
-
-      page.render('screenshots/test' + testTitle + '.png');
-
-      //Inject our libraries into the page
-      page.injectJs("../lib/jquery-1.6.4.min.js");
-      page.injectJs("../lib/cssert.js");
-
-      //Inject variables into the page context
-      eval("function fn() { stylesToAssert = JSON.parse('" + JSON.stringify(stylesToAssert).replace(/'/g, "\\'") + "');  testSubject = $('" + testSubject + "'); testTitle = '" + testTitle + "'}");
-      page.evaluate(fn);
-
-      //Run the actual test case
-      page.evaluate(function() {
-        console.log("--");
-        if (CSSERT.assertStyles(testSubject, stylesToAssert)) {
-          console.log('\033[0;32m' + testTitle + ' : Passed\033[0;37m');
-        } else {
-          console.log('\033[0;31m' + testTitle + ' : Failed\033[0;37m');
-        }
-      });
-
-    } else {
-      console.log('Failed to open test page');
-    }
-
-    // Tidy up after ourselves
-    fs.remove(filename);
-
-    //This prevents PhantomJS quitting before we've run each individual test case.
-    if (outstandingTests-- <= 0) {
-      phantom.exit();
-    }
-
-    page.release();
   };
 
-  page.open(filename);
-};
+  // Create the test files and begin running the tests
+  page.open(phantom.args[0], init);
 
-// This is an odd workaround for the asynchronous page.open behaviour
-// Open the file, do nothing with it.
-// then open it again and perform the actual tests.
-beginTest = function(filename) {
-  var page = require('webpage').create();
-  page.onLoadFinished = function(status) {
-    //This is to get round the asynchronous open calls
-    if (outstandingTests-- <= 0) {
-      runTheTests();
-    }
-    page.release();
+  runTest = function(filename, testSubject, stylesToAssert, testTitle) {
+    log('runTest :: ' + testTitle, 3);
+    var testPage = require('webpage').create();
+
+    // Arbitrary. Can be changed but suits most purposes.
+    testPage.viewportSize = {
+      width: 1600,
+      height: 1600
+    };
+    testPage.onConsoleMessage = consoleMessage;
+
+    testPage.onLoadStarted = function() {};
+
+    testPage.onLoadFinished = function(status) {
+      if (status === "success") {
+
+        testPage.render('screenshots/test' + testTitle + '.png');
+
+        //Inject our libraries into the page
+        testPage.injectJs("../lib/jquery-1.6.4.min.js");
+        testPage.injectJs("../lib/cssert.js");
+
+        // Create a temporary JS file to inject variables into the page context
+        var testData = "stylesToAssert = JSON.parse('" + JSON.stringify(stylesToAssert).replace(/'/g, "\\'") + "');" + "testSubject = $('" + testSubject + "');" + "testTitle = '" + testTitle + "'";
+        var dataPath = fs.workingDirectory + fs.separator + 'data.js';
+        var dataFile = fs.open(fs.workingDirectory + fs.separator + 'data.js', 'w');
+        dataFile.write(testData);
+        dataFile.close();
+        testPage.injectJs(dataPath);
+
+        //Run the actual test case
+        testPage.evaluate(function() {
+          console.log("--");
+          // console.log($('html').html());
+          if (CSSERT.assertStyles(testSubject, stylesToAssert)) {
+            console.log('\033[0;32m' + testTitle + ' : Passed\033[0;37m');
+          } else {
+            console.log('\033[0;31m' + testTitle + ' : Failed\033[0;37m');
+          }
+        });
+
+        //Remove test data
+        fs.remove(dataPath);
+
+
+      } else {
+        log('Failed to open test page');
+      }
+
+      // Tidy up after ourselves
+      fs.remove(filename);
+
+      //This prevents PhantomJS quitting before we've run each individual test case.
+      if (outstandingTests-- <= 0) {
+        phantom.exit();
+      }
+
+      testPage.release();
+    };
+
+    testPage.open(filename);
   };
 
-  page.open(filename);
-};
+  // This is an odd workaround for the asynchronous page.open behaviour
+  // Open the file, do nothing with it.
+  // then open it again and perform the actual tests.
+  bufferTest = function(filename) {
+    var page = require('webpage').create();
+    page.onLoadFinished = function(status) {
+      //This is to get round the asynchronous open calls
+      if (outstandingTests-- <= 0) {
+        runTheTests();
+      }
+      page.release();
+    };
+
+    page.open(filename);
+  };
 
 
-// Once we've created all the files, open them again and do the actual testing
-runTheTests = function() {
-  outstandingTests = tests.length - 1;
-  for (var a = 0; a < tests.length; a++) {
-    testObject = tests[a];
-    runTest('test-' + testObject.title + '.html', testObject.selector, JSON.parse(testObject.styles), testObject.title);
-  }
-}
+  // Once we've created all the files, open them again and do the actual testing
+  runTheTests = function() {
+    outstandingTests = tests.length - 1;
+    for (var a = 0; a < tests.length; a++) {
+      runTest('test-' + tests[a].title + '.html', tests[a].selector, JSON.parse(tests[a].styles), tests[a].title);
+    }
+  };
+})();
